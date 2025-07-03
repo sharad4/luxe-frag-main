5edb55c86431a1d06ef3f7b1cc9b45d23b051574
Sharad <sharad011989@gmail.com>
2025-07-04 00:15:09 +0530

    add

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import ProductCard from './ProductCard';
 
 const ProductList = ({ products, setSelectedProduct, addToCart }) => {
   return (
-    <div>ProductList</div>
-  )
-}
+    <div className='grid md:grid-cols-2 gap-8'>
+        {products.map((product) => (
+            <ProductCard
+                key={product.id}
+                product={product}
+                setSelectedProduct={setSelectedProduct}
+                addToCart={addToCart}
+                showActions={true} 
+            />
+        ))}
+    </div>
+  );
+};
 
 export default ProductList
